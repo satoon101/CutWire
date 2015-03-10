a5fa873702d5b87707fc4e3f2008fef9f25b64eb
Stephen Toon <satoon101@gmail.com>
2015-03-10 16:10:52 -0400

    Updated .gitignore to ignore server specific translations.
    Added comments to the plugin.

--- addons/source-python/plugins/cut_wire/cut_wire.py
+++ addons/source-python/plugins/cut_wire/cut_wire.py
@@ -2,7 +2,6 @@
 
 """Allows bomb defusers to choose a wire to cut."""
 
-
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
@@ -36,24 +35,30 @@ from cut_wire.info import info
 # =============================================================================
 # >> GLOBAL VARIABLES
 # =============================================================================
+# Get the in-game strings to use
 wire_strings = LangStrings(info.basename + '/strings')
 
+# Get the config strings to use
 config_strings = LangStrings(info.basename + '/config_strings')
 
+# Store the wire color choices
 _colors = ('Blue', 'Yellow', 'Red', 'Green')
 
 
 # =============================================================================
 # >> CONFIGURATION
 # =============================================================================
+# Create the config file
 with ConfigManager(info.basename) as config:
 
+    # Create the send menu convar
     send_menu = config.cvar(
         'cw_send_menu', 1, ConVarFlags.NONE, config_strings['SendMenu'])
     for _option in range(1, 4):
         send_menu.Options.append(
             config_strings['MenuOption{0}'.format(_option)])
 
+    # Create the bot convar
     bot_choose_wire = config.cvar(
         'cw_bot_choose_wire', 0, ConVarFlags.NONE, config_strings['BotChoice'])
     for _option in range(3):
@@ -67,31 +72,42 @@ with ConfigManager(info.basename) as config:
 @Event
 def bomb_begindefuse(game_event):
     """Send a menu to the defuser."""
+    # Get the defuser
     player = PlayerEntity(index_from_userid(game_event.get_int('userid')))
 
+    # Get the bomb's instance
     for bomb in EntityIter('planted_c4', return_types='entity'):
-
         break
 
+    # Get whether the defuser has time to defuse
     gonna_blow = bomb.defuse_length > bomb.timer_length
 
+    # Is the defuser a bot?
     if player.is_fake_client():
 
+        # Get the bot convar's value
         bot_setting = bot_choose_wire.get_int()
 
+        # Should the bot cut a wire?
         if (bot_setting == 1 and gonna_blow) or bot_setting == 2:
 
+            # Cut a wire
             cut_chosen_wire(choice(_colors))
 
+        # No need to go further
         return
 
+    # Get the send menu convar's value
     send_setting = send_menu.get_int()
 
+    # Get whether the defuser has a kit
     has_kit = game_event.get_bool('haskit')
 
+    # Should the wire cut menu be sent to the defuser?
     if (send_setting == 1 or (send_setting == 2 and gonna_blow)
             or (send_setting == 3 and (gonna_blow or not has_kit))):
 
+        # Send the wire cut menu to the defuser
         wire_menu.send(player.index)
 
 
@@ -112,11 +128,14 @@ def bomb_choice(menu, index, option):
 # =============================================================================
 # >> MENU CREATION
 # =============================================================================
+# Create the wire cut menu
 wire_menu = PagedMenu(
     description=wire_strings['Title'], select_callback=bomb_choice)
 
+# Loop through all choices of wire colors
 for _color in _colors:
 
+    # Add the color to the menu
     wire_menu.append(PagedOption(wire_strings[_color], _color))
 
 
@@ -125,16 +144,18 @@ for _color in _colors:
 # =============================================================================
 def cut_chosen_wire(chosen_wire):
     """Cut a wire to defuse or explode the bomb."""
+    # Get the bomb's instance
     for bomb in EntityIter('planted_c4', return_types='entity'):
-
         break
 
+    # Did the defuser choose the correct wire?
     if chosen_wire == choice(_colors):
 
-        print('defusing')
+        # Defuse the bomb
         bomb.defuse_count_down = 0.0
 
+    # Did the defuser choose one of the wrong wires?
     else:
 
-        print('exploding')
+        # Explode the bomb
         bomb.c4_blow = 0.0

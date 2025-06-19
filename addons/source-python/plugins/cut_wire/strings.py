d99ffbcd126ee713e89c517292bcec5e3b8c87b4
satoon101 <satoon101@gmail.com>
2025-06-19 18:35:13 -0400

    Minor changes.

--- addons/source-python/plugins/cut_wire/strings.py
+++ addons/source-python/plugins/cut_wire/strings.py
@@ -1,6 +1,6 @@
 # ../cut_wire/strings.py
 
-"""Contains all translation variables for the base plugin."""
+"""Contains all translation variables for the plugin."""
 
 # =============================================================================
 # >> IMPORTS
